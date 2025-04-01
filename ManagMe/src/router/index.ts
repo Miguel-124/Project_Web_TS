cd696c5610abbeb7832a2542f216525f36dff218
michalgorecki <michalgorecki124@gmail.com>
2025-04-01 14:41:55 +0200

    fix(router): update import path for KanbanView to TaskKanbanView

--- ManagMe/src/router/index.ts
+++ ManagMe/src/router/index.ts
@@ -4,7 +4,7 @@ import ProjectListView from '@/views/ProjectListView.vue'
 import StoryListView from '@/views/StoryListView.vue'
 import TaskListView from '@/views/TaskListView.vue'
 import TaskDetailView from '@/views/TaskDetailView.vue'
-import KanbanView from '@/views/KanbanView.vue'
+import KanbanView from '@/views/TaskKanbanView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
